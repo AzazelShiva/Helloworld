22eeb261355d466913bb1e6ccacd75fb11bf11b3
Azazelanky <marvelsks@yandex.ru>
2019-03-07 01:24:58 +0300

    Задача №10: Ввод/вывод данных для матрицы

---
+++ src/Task_10.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Task_10 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Необходимо ввести размер матрицы, сначала введем количество строк, затем количество столбцов: ");
+        int rows = input.nextInt(); // Читаем с клавиатуры количество строк и записываем в rows
+        int cols = input.nextInt(); // Читаем с клавиатуры количество столбцов и записываем в cols
+        int array[][] = new int[rows][cols]; // Создаём матрицу размером в rows*cols
+        System.out.println("Необходимо ввести элементы матрицы");
+
+        /*Пробежимся по всей матрице и заполним ее*/
+        for (int i = 0; i <rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print("Необходимо ввести элемент [" + i + "][" + j + "]:");
+                array[i][j] = input.nextInt();
+            }
+        }
+        System.out.print ("Вывод первой строки матрицы, в котором каждый элемент умножен на 3: \n");
+        for (int j = 0; j < cols; j++) {
+            System.out.print(array[0][j]*3 + " ");
+        }
+        System.out.println();
+    }
+}
