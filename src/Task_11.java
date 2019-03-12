c574e2c4490ca6024d27b8085bee9914837b9ef0
Azazelanky <marvelsks@yandex.ru>
2019-03-12 23:36:13 +0300

    Задача №11: Ввод строки с клавиатуры

---
+++ src/Task_11.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Task_11 {
+    public static void main(String[] args) {
+
+        Scanner l = new Scanner(System.in);
+        System.out.println("Необходимо ввести строку:");
+        String str = l.nextLine();
+
+        System.out.println( "Вы ввели:\n" + str);
+
+    }
+}
